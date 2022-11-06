fe79d9e2401701d7df4d96dff22f9c8e531fc822
onyangodonomondi <onyangodonomondi@gmail.com>
2022-11-06 17:42:18 +0300

    update

--- user_management/settings.py
+++ user_management/settings.py
@@ -84,10 +84,16 @@ TEMPLATES = [
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'gbLnQrVRBz3Mjwsncbm2',
+        'HOST': 'containers-us-west-16.railway.app',
+        'PORT': '7292',
     }
 }
+18
+
 
 
 # Password validation
